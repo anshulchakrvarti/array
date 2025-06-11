6aa0172f4bfdda58585ec869a9ecf97cbd4a9f70
ANSHUL CHAKRVARTI <anshulchakrawaeti@gmail.com>
2025-06-11 10:22:06 +0530

    write a program to copy the xontents of onw array into another in the reverse order

---
+++ arraycopy.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // write a program to copy the xontents of onw array into another in the reverse order;
+int arr[5]={1,2,3,4,5};
+int brrd[5];
+int x=5-1;
+for (int i =0; i <5; i++)
+{ 
+     brrd[i]=arr[x];
+      cout<<" "<<brrd[i];
+     x--;
+}
+ 
+
+    return 0;
+}
