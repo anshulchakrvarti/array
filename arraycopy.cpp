3e50f74bcbe9a9b8b5874d74ab627c96d763a97e
ANSHUL CHAKRVARTI <anshulchakrawaeti@gmail.com>
2025-06-11 15:19:04 +0530

    Add files via upload

--- arraycopy.cpp
+++ arraycopy.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    // write a program to copy the xontents of onw array into another in the reverse order;
+    // write a program to copy the contents of one array into another in the reverse order;
 int arr[5]={1,2,3,4,5};
 int brrd[5];
 int x=5-1;
